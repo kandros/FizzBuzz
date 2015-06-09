2c7c6bf99f6884c57d208e9bd593fbe511a90297
kandros <kandros5591@gmail.com>
2015-06-09 08:37:00 +0200

    map's key are now integers, so theres no need to cast them when passing them to multiplesOf.

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,3 +1,4 @@
+/*jshint esnext : true */ // suppress jshint warning of using es6
 // Get argument passed while executing the program with Node.js, as actual arguments are from 3rd ownard
 var arg = process.argv[2];
 
@@ -6,22 +7,20 @@ function FizzBuzz(number) {
 
   var result = "";
   var knownNumberStringPairs = new Map([
-    ["3","Fizz"],
-    ["5","Buzz"],
-    ["7","Mezz"],
-    ["22","Lotzz"],
+    [ 3 ,"Fizz"],
+    [ 5 ,"Buzz"],
+    [ 7 ,"Mezz"],
+    [22 ,"Lotzz"],
   ]);
 
   function multipliedByParser(){
     var map = knownNumberStringPairs;
     for (var key of map.keys()) {
-      var keyN = Number(key);
-      if (multipleOf(keyN)) {
+      if (multipleOf(key)) {
         result += map.get(key);
       }
     }
   }
-
 
   function multipleOf(num){
     return number % num === 0;
